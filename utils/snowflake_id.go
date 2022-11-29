4fa5c1c61bd36afb2f7f1a25951e999ce122277a
hjfu <supertain147@163.com>
2022-11-30 00:16:31 +0800

    添加雪花算法

--- utils/snowflake_id.go
+++ utils/snowflake_id.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/bwmarrin/snowflake"
 )
 
@@ -13,23 +10,14 @@ func GenId() int64 {
 	return node.Generate().Int64()
 }
 
-func Init(startTime string, machineId int64) (err error) {
-	var st time.Time
-	st, err = time.Parse("2006-01-02", startTime)
-	if err != nil {
-		return err
-	}
-	fmt.Println("st:", st)
-	snowflake.Epoch = st.UnixNano() / 1000000
+func Init(machineId int64) (err error) {
+	// var st time.Time
+	// st, err = time.Parse("2006-01-02", startTime)
+	// if err != nil {
+	// 	return err
+	// }
+	// fmt.Println("st:", st)
+	// snowflake.Epoch = st.UnixNano() / 1000000
 	node, err = snowflake.NewNode(machineId)
 	return err
-}
-
-func main() {
-	if err := Init(time.Now().Format("2006-01-02"), 1); err != nil {
-		fmt.Printf("init failed ,err:%v\n", err)
-	}
-	id := GenId()
-	fmt.Println(id)
-
 }
